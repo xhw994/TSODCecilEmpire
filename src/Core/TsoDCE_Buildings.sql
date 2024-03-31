4151c33d5ebb240a7f2f2fc8fde51ad9a5ac256c
xhw994 <wxh960302@gmail.com>
2024-03-30 22:39:09 -0700

    feat: Mana obelisk base yield, and double yield after electricity

---
+++ src/Core/TsoDCE_Buildings.sql
@@ -0,0 +1,145 @@
+INSERT INTO
+    Types (Type, Kind)
+VALUES
+    ('BUILDING_TSOD_MANA_OBELISK', 'KIND_BUILDING');
+
+INSERT INTO
+    BuildingReplaces (CivUniqueBuildingType, ReplacesBuildingType)
+VALUES
+    ('BUILDING_TSOD_MANA_OBELISK', 'BUILDING_MONUMENT');
+
+INSERT INTO
+    Buildings (BuildingType, Name, Description, PrereqDistrict, PurchaseYield, Cost, AdvisorType, PrereqTech, TraitType, Entertainment)
+VALUES
+    (
+        'BUILDING_TSOD_MANA_OBELISK',
+        'LOC_BUILDING_TSOD_MANA_OBELISK_NAME',
+        'LOC_BUILDING_TSOD_MANA_OBELISK_DESCRIPTION',
+        'DISTRICT_CITY_CENTER',
+        'YIELD_GOLD',
+        60,
+        'ADVISOR_CULTURE',
+        'TECH_MATHEMATICS',
+        'TRAIT_BUILDING_TSOD_MANA_OBELISK',
+        1
+    );
+
+INSERT INTO
+    Building_YieldChanges (BuildingType, YieldType, YieldChange)
+VALUES
+    ('BUILDING_TSOD_MANA_OBELISK', 'YIELD_SCIENCE', 2),
+    ('BUILDING_TSOD_MANA_OBELISK', 'YIELD_PRODUCTION', 2);
+
+-- District adjacency bonus
+-- CREATE TABLE TsoDDistrictYields (District TEXT NOT NULL, YieldType TEXT NOT NULL, PRIMARY KEY (District));
+-- INSERT INTO
+--     TsoDDistrictYields (District, YieldType)
+-- VALUES
+--     ('HOLY_SITE', 'FAITH'),
+--     ('CAMPUS', 'SCIENCE'),
+--     ('HARBOR', 'GOLD'),
+--     ('COMMERCIAL_HUB', 'GOLD'),
+--     ('THEATER', 'CULTURE'),
+--     ('INDUSTRIAL_ZONE', 'PRODUCTION'),
+--     ('ENCAMPMENT', 'PRODUCTION'),
+--     ('PRESERVE', 'FOOD');
+-- INSERT INTO
+--     BuildingModifiers (BuildingType, ModifierId)
+-- VALUES
+--     ('BUILDING_TSOD_MANA_OBELISK', 'TSOD_MANA_OBELISK_' || District || '_ADJACENCY')
+-- FROM
+--     TsoDDistrictYields;
+-- INSERT INTO
+--     Modifiers (ModifierId, ModifierType)
+-- SELECT
+--     'TSOD_MANA_OBELISK_' || District ||         '_ADJACENCY',
+--     'MODIFIER_PLAYER_CITIES_DISTRICT_ADJACENCY'
+-- FROM
+--     TsoDDistrictYields;
+-- INSERT INTO
+--     ModifierArguments (ModifierId, Name, VALUE)
+-- SELECT
+--     'TSOD_MANA_OBELISK_' || t.District || '_ADJACENCY' AS ModifierId,
+--     CASE
+--         WHEN x.Name = 'Amount' THEN '1'
+--         WHEN x.Name = 'Description' THEN 'LOC_DISTRICT_DISTRICT_1_' || t.YieldType
+--         WHEN x.Name = 'DistrictType' THEN 'DISTRICT_' || t.District
+--         WHEN x.Name = 'YieldType' THEN 'YIELD_' || t.YieldType
+--     END AS VALUE,
+--     x.Name
+-- FROM
+--     TsoDDistrictYields t,
+--     (
+--         SELECT
+--             'Amount' AS Name
+--         UNION ALL
+--         SELECT
+--             'Description'
+--         UNION ALL
+--         SELECT
+--             'DistrictType'
+--         UNION ALL
+--         SELECT
+--             'YieldType'
+--     ) x;
+
+-- Building double yield
+INSERT INTO
+    BuildingModifiers (BuildingType, ModifierId)
+SELECT
+    'BUILDING_TSOD_MANA_OBELISK',
+    'TSOD_MANA_OBELISK_DOUBLE_YIELD_' || BuildingType || '_' || YieldType
+FROM
+    Building_YieldChanges;
+
+INSERT INTO
+    Modifiers (ModifierId, ModifierType, OwnerRequirementSetId)
+SELECT
+    'TSOD_MANA_OBELISK_DOUBLE_YIELD_' || BuildingType || '_' || YieldType,
+    'MODIFIER_PLAYER_CITIES_ADJUST_BUILDING_YIELD_CHANGE',
+    'PLAYER_HAS_TECH_ELECTRICITY_REQUIREMENTS'
+FROM
+    Building_YieldChanges;
+
+INSERT INTO
+    ModifierArguments (ModifierId, Name, VALUE)
+SELECT
+    'TSOD_MANA_OBELISK_DOUBLE_YIELD_' || BuildingType || '_' || YieldType,
+    'YieldType',
+    YieldType
+FROM
+    Building_YieldChanges
+UNION
+SELECT
+    'TSOD_MANA_OBELISK_DOUBLE_YIELD_' || BuildingType || '_' || YieldType,
+    'BuildingType',
+    BuildingType
+FROM
+    Building_YieldChanges
+UNION
+SELECT
+    'TSOD_MANA_OBELISK_DOUBLE_YIELD_' || BuildingType || '_' || YieldType,
+    'Amount',
+    YieldChange
+FROM
+    Building_YieldChanges;
+
+INSERT INTO
+    RequirementSets (RequirementSetId, RequirementSetType)
+VALUES
+    ('PLAYER_HAS_TECH_ELECTRICITY_REQUIREMENTS', 'REQUIREMENTSET_TEST_ALL');
+
+INSERT INTO
+    RequirementSetRequirements (RequirementSetId, RequirementId)
+VALUES
+    ('PLAYER_HAS_TECH_ELECTRICITY_REQUIREMENTS', 'PLAYER_HAS_TECH_ELECTRICITY_REQUIREMENT');
+
+INSERT INTO
+    Requirements (RequirementId, RequirementType)
+VALUES
+    ('PLAYER_HAS_TECH_ELECTRICITY_REQUIREMENT', 'REQUIREMENT_PLAYER_HAS_TECHNOLOGY');
+
+INSERT INTO
+    RequirementArguments (RequirementId, Name, VALUE)
+VALUES
+    ('PLAYER_HAS_TECH_ELECTRICITY_REQUIREMENT', 'TechnologyType', 'TECH_ELECTRICITY');
--- src/Texts/TsoDCE_BuildingTexts.sql
+++ src/Texts/TsoDCE_BuildingTexts.sql
@@ -1,25 +1,20 @@
 INSERT INTO
-    LocalizedText (Tag, Language, Text)
+    LocalizedText (Tag, LANGUAGE, Text)
 VALUES
-    (
-        'LOC_BUILDING_TSOD_MANA_OBELISK_NAME',
-        'zh_Hans_CN',
-        '魔能方尖碑'
-    ),
+    ('LOC_BUILDING_TSOD_MANA_OBELISK_NAME', 'zh_Hans_CN', '魔能方尖碑'),
     (
         'LOC_BUILDING_TSOD_MANA_OBELISK_DESCRIPTION',
         'zh_Hans_CN',
-        '魔能方尖碑的介绍'
-    );
+        '塞西尔的独特建筑，替代纪念碑和旧神纪念碑。提供+2[ICON_Science]科技、+2[ICON_Production]生产力和+1[ICON_Amenities]宜居度。为所有区域提供额外的标准相邻加成。研究电力后为所有建筑的基础产能翻倍。'
+    ),
+    ('LOC_DISTRICT_DISTRICT_1_FOOD', 'zh_Hans_CN', '+1 [ICON_Food] 食物来自相邻区域。');
 
 INSERT INTO
     EnglishText (Tag, Text)
 VALUES
-    (
-        'LOC_BUILDING_TSOD_MANA_OBELISK_NAME',
-        'Mana Obelisk'
-    ),
+    ('LOC_BUILDING_TSOD_MANA_OBELISK_NAME', 'Mana Obelisk'),
     (
         'LOC_BUILDING_TSOD_MANA_OBELISK_DESCRIPTION',
-        'Description of Mana Obelisk.'
-    );
+        'Cecil''s unique building, replacing Monuments and Old God Obelisks. Provides +2[ICON_Science] Science, +2[ICON_Production] Production, and +1[ICON_Amenities] Amenity. All districts receive an additional standard adjacency bonus. After researching Electricity, doubles the base yield of all buildings.'
+    ),
+    ('LOC_DISTRICT_DISTRICT_1_FOOD', '+1[ICON_Food] Food from the adjacent districts.');
