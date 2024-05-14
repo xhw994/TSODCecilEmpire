1387e437e278aa548aecede301454d6c2ce9c56b
xhw994 <wxh960302@gmail.com>
2024-05-13 23:29:35 -0700

    feat: Gorgon platform combat bonus

--- src/Texts/TsoDCE_UnitTexts.sql
+++ src/Texts/TsoDCE_UnitTexts.sql
@@ -52,8 +52,10 @@ VALUES
     (
         'LOC_UNIT_TSOD_GORGON_PLATFORM_DESCRIPTION',
         'zh_Hans_CN',
-        '塞西尔的独特支援单位，替代无人机。为2单元格内的攻城单位+1[ICON_Range]射程，+5[ICON_Strength]攻击力。为两格内的所有单位提供15点回复。两格内的所有改良获得额外产出。'
-    );
+        '塞西尔的独特支援单位，替代无人机。为1单元格内的攻城单位+1[ICON_Range]射程，+5[ICON_Strength]攻击力。能治疗邻近单位。两格内的所有改良获得额外产出。'
+    ),
+    ('LOC_TSOD_GORGON_PLATFORM_SIEGE_BONUS_DESCRIPTION', 'zh_Hans_CN', '相邻戈尔贡平台的攻城单位+5[ICON_Strength]攻击力。'),
+    ('LOC_TSOD_GORGON_PLATFORM_SIEGE_BONUS_MODIFIER', 'zh_Hans_CN', '相邻戈尔贡平台+{1_Amount}战斗力。');
 
 INSERT INTO
     EnglishText (Tag, Text)
@@ -125,5 +127,10 @@ VALUES
     ('LOC_UNIT_TSOD_GORGON_PLATFORM_NAME', 'Gorgon Platform'),
     (
         'LOC_UNIT_TSOD_GORGON_PLATFORM_DESCRIPTION',
-        'Cecil''s unique support unit, replaces drones. Provides +1 [ICON_Range] range and +5 [ICON_Strength] combat strength to siege units within 2 cells. Heals all units within two cells by 15 points. All improvements within two cells gain extra output.'
-    );
+        'Cecil''s Atomic era support unit, replaces drone. Adds +1 [ICON_Range] range and +5 [ICON_Strength] bombard strength to siege units within 1 hex. Can heal adjacent units. Adjacent improvements gain bonus yield.'
+    ),
+    (
+        'LOC_TSOD_GORGON_PLATFORM_SIEGE_BONUS_DESCRIPTION',
+        '+1 [ICON_Range] range and +5 [ICON_Strength] bombard strength to siege units adjacent to Gorgon Platform.'
+    ),
+    ('LOC_TSOD_GORGON_PLATFORM_SIEGE_BONUS_MODIFIER', '+{1_Amount} bombard strength adjacent to Gorgon Platform.');
