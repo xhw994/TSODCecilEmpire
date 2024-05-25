29529c46e4c4a7865bd643ac5d31288dfe88c5b5
xhw994 <wxh960302@gmail.com>
2024-05-25 03:13:22 -0700

    text: Add English for buildings

--- src/Texts/TsoDCE_BuildingTexts.sql
+++ src/Texts/TsoDCE_BuildingTexts.sql
@@ -8,8 +8,16 @@ VALUES
         '塞西尔的独特建筑，替代纪念碑。提供+2[ICON_Science]科技、+2[ICON_Production]生产力和+1[ICON_Amenities]宜居度。为所有区域提供额外的标准相邻加成。研究电力后为所有建筑的基础产能翻倍。'
     ),
     ('LOC_DISTRICT_DISTRICT_1_FOOD', 'zh_Hans_CN', '+1 [ICON_Food] 食物来自相邻区域。'),
-    ('LOC_PEDIA_BUILDINGS_PAGE_BUILDING_TSOD_MANA_OBELISK_CHAPTER_HISTORY_PARA_1', 'zh_Hans_CN', '现代魔导理论起源于不明姓名的野法师，经过詹尼与拉文凯斯等人的完善，采用数理逻辑探究魔力基础规律，结合机械学等学科并进行符文研究与优化、实践的理论。'),
-    ('LOC_PEDIA_BUILDINGS_PAGE_BUILDING_TSOD_MANA_OBELISK_CHAPTER_HISTORY_PARA_2', 'zh_Hans_CN', '经过数理逻辑优化的简单符文组合，塞西尔研发出了魔网科技。它聚合自然游离魔力的基础自持魔导装置，可以对外输出能量和传递信息，可进行多区域并网。类似地球科技中的发电机、电网与通讯网络的结合体，但不需要额外投入能源物质。'),
+    (
+        'LOC_PEDIA_BUILDINGS_PAGE_BUILDING_TSOD_MANA_OBELISK_CHAPTER_HISTORY_PARA_1',
+        'zh_Hans_CN',
+        '现代魔导理论起源于不明姓名的野法师，经过詹尼与拉文凯斯等人的完善，采用数理逻辑探究魔力基础规律，结合机械学等学科并进行符文研究与优化、实践的理论。'
+    ),
+    (
+        'LOC_PEDIA_BUILDINGS_PAGE_BUILDING_TSOD_MANA_OBELISK_CHAPTER_HISTORY_PARA_2',
+        'zh_Hans_CN',
+        '经过数理逻辑优化的简单符文组合，塞西尔研发出了魔网科技。它聚合自然游离魔力的基础自持魔导装置，可以对外输出能量和传递信息，可进行多区域并网。类似地球科技中的发电机、电网与通讯网络的结合体，但不需要额外投入能源物质。'
+    ),
     ('LOC_PEDIA_BUILDINGS_PAGE_BUILDING_TSOD_MANA_OBELISK_CHAPTER_HISTORY_PARA_3', 'zh_Hans_CN', '魔能方尖碑可以让魔力在魔网周边一定范围内使用，可传递信息，可以类比wifi与无线输电装置的综合体。');
 
 INSERT INTO
@@ -18,6 +26,18 @@ VALUES
     ('LOC_BUILDING_TSOD_MANA_OBELISK_NAME', 'Mana Obelisk'),
     (
         'LOC_BUILDING_TSOD_MANA_OBELISK_DESCRIPTION',
-        'Cecil''s unique building, replacing Monuments. Provides +2[ICON_Science] Science, +2[ICON_Production] Production, and +1[ICON_Amenities] Amenity. All districts receive an additional standard adjacency bonus. After researching Electricity, doubles the base yield of all buildings.'
+        'Cecil''s unique building, replacing Monuments. Provides +2[ICON_Science] Science, +2[ICON_Production] Production, and +1 [ICON_Amenities] Amenity. All districts receive an additional standard adjacency bonus. After researching Electricity, doubles the base yield of all buildings.'
     ),
-    ('LOC_DISTRICT_DISTRICT_1_FOOD', '+1[ICON_Food] Food from the adjacent districts.');
+    ('LOC_DISTRICT_DISTRICT_1_FOOD', '+1[ICON_Food] Food from the adjacent districts.'),
+    (
+        'LOC_PEDIA_BUILDINGS_PAGE_BUILDING_TSOD_MANA_OBELISK_CHAPTER_HISTORY_PARA_1',
+        'Modern magitech theory originated from an unknown wild mage. It was refined by people like Jenny Pero and Ravenkiss by using math to reason the basic laws of magic. Later, Cecil leveraged and introduced magitech to mechanics, and conducted the research, optimization, and practical application of magical runes.'
+    ),
+    (
+        'LOC_PEDIA_BUILDINGS_PAGE_BUILDING_TSOD_MANA_OBELISK_CHAPTER_HISTORY_PARA_2',
+        'With simple rune combinations optimized through mathematical logic, Cecil developed maginet technology. It aggregates naturally free magic power into a self-sustaining magitech device, capable of outputting energy and transmitting information externally, and can be networked across multiple areas. It is similar to the combination of a generator, power grid, and communication network on Earth but does not require additional energy input.'
+    ),
+    (
+        'LOC_PEDIA_BUILDINGS_PAGE_BUILDING_TSOD_MANA_OBELISK_CHAPTER_HISTORY_PARA_3',
+        'The mana obelisk allows the use of magic power within a certain range around the maginet. It can transmit information, similar to a combination of Wi-Fi and wireless power transmission devices.'
+    ),;
